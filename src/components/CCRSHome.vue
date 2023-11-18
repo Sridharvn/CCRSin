547367ebaa9f1d45d3580d29dad0a5d1f2e302f5
Sridharvn <sridharvnampoothiripad@gmail.com>
2023-11-18 13:56:22 +0530

    Added 2024 oncology

--- src/components/CCRSHome.vue
+++ src/components/CCRSHome.vue
@@ -1,22 +1,36 @@
 <template>
   <div id="home">
     <main>
-      <h1>
-        Cancer Control & Research Society (CCRS)
-      </h1>
-      
-      <h3>Cancer Control & Research Society is a charitable organisation registered as per Act XII of 1955. The society is involved in cancer control activities by providing cancer care, palliative home care, assistance to suffering cancer patients, cancer awareness camps, continuing medical education programmes and international and national multidisciplinary oncology conferences.</h3>
-      
-      <br>
+      <h1>Cancer Control & Research Society (CCRS)</h1>
+
+      <!-- <h3>Cancer Control & Research Society is a charitable organisation registered as per Act XII of 1955. The society is involved in cancer control activities by providing cancer care, palliative home care, assistance to suffering cancer patients, cancer awareness camps, continuing medical education programmes and international and national multidisciplinary oncology conferences.</h3> -->
+      <h3>
+        Cancer Control & Research Society is an organisation registered under
+        charitable societies Act XII of 1955. It is involved in provision of
+        palliative care and home care for suffering cancer patients. This
+        society is organising cancer detection camps, health awareness
+        programmes, continuing medical and & nursing education programmes,
+        international and national oncology conferences for medical doctors,
+        paramedical personnels, nurses and pharmasists. Financial aid to
+        suffering cancer patients for undergoing treatment is another major
+        activity of this organisation. We could address the government,
+        political leaders and organisations about the pressing problems in
+        cancer care and how it can be improved further.
+      </h3>
+
+      <br />
     </main>
     <aside>
-      <img src="./../assets/MSHS-Cancer-RadiationOncology-2col-685x375-3.jpg" alt="" style="max-height: 90vh; max-width: 55vw;" />
+      <img
+        src="./../assets/MSHS-Cancer-RadiationOncology-2col-685x375-3.jpg"
+        alt=""
+        style="max-height: 90vh; max-width: 55vw"
+      />
       <!-- <img src="./../assets/317582_1100-732x549.jpg" alt="" style="max-height: 90vh; max-width: 55vw;" /> -->
     </aside>
   </div>
 </template>
-<script>
-</script>
+<script></script>
 <style scoped>
 #home {
   display: flex;
@@ -38,7 +52,7 @@ main {
 
 #home {
   background-color: var(--primary-color);
-  color: var(--secondary-color)
+  color: var(--secondary-color);
 }
 
 @media screen and (max-width: 800px) {
